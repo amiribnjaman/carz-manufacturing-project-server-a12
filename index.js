781117abf4b2615aaefebb9b263685a7c87c2bac
amiribnjaman <amiribnjaman@gmail.com>
2022-05-23 21:47:15 +0600

    Mongo db connected

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
+require('dotenv').config()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.go3nt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// Run or initial for mongo function
+(async () => {
+    app.get('/hello', (req, res) => {
+        res.json({ "say": "hello" })
+    })
+
+})().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
