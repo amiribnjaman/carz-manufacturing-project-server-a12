f98b703f3ce007086c62fcc59607803cd2a9f36a
amiribnjaman <amiribnjaman@gmail.com>
2022-05-23 22:20:53 +0600

    Insert or update a user api endpoint created

--- index.js
+++ index.js
@@ -13,13 +13,33 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-// Run or initial for mongo function
+// Run or initial function for mongodb
 (async () => {
-    app.get('/hello', (req, res) => {
-        res.json({ "say": "hello" })
-    })
+    try {
+        await client.connect();
+        const userCollection = client.db("carz_manufacturing_a12").collection("users");
+
+
+        // Update or insert a user's api endpoint
+        app.put('/user', async (req, res) => {
+            const data = req.body;
+            console.log(data);
+            const query = { email: data.email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: data
+            }
+            const result = await userCollection.updateOne(query, updateDoc, options)
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
 
 })().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
