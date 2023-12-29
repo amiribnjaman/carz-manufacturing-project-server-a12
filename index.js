626090a31886383c31371a9f523d87fda17d2e6e
Amir Hosen <75005484+amiribnjaman@users.noreply.github.com>
2023-12-29 21:42:06 +0600

    Database issue resolved

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
 app.use(express.json())
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.go3nt.mongodb.net/carz_manufacturing_a12?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.go3nt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
