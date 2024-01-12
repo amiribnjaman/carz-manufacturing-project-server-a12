8e168accf8dd274af586960b0a1b71849628c245
amiribnjaman <amiribnjaman@gmail.com>
2024-01-12 12:29:20 +0600

    App file created

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// Application level Middlewares
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Testing Route
+app.get("/", (req, res) => {
+  res.status(200).send({ msg: "Hello world" });
+});
+
+// Hanlde Url Error
+app.use((req, res, next) => {
+  res.status(404).send({
+    message: "Bad request",
+  });
+});
+
+// Hanlde Server Errro
+app.use((errro, req, res, next) => {
+  res.status(500).send({
+    message: "Internal problem",
+  });
+});
+
+module.exports = app;
